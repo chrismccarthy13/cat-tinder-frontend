c93b9c623bcd6d707ec8c36b36bde3350ace9883
learn <learn@learn16.local>
2019-07-25 11:36:54 -0700

    initial commit

--- cat-tinder-frontend/src/App.js
+++ cat-tinder-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 // import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+// import {getCats} from './api/index'
 import NavBar from './components/NavBar'
 
 
